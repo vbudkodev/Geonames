70dd9511a42e0cbfd9af036fc43059831b914579
vbudko <budko@pktbcdt.rzd.ru>
2025-09-17 16:00:37 +0300

    [fix] autoload

--- src/GeonamesServiceProvider.php
+++ src/GeonamesServiceProvider.php
@@ -16,9 +16,6 @@ class GeonamesServiceProvider extends \Illuminate\Support\ServiceProvider {
 
         // There are a number of tables that need to be created for our Geonames package.
         // Feel free to create your own additional migrations to create indexes that are appropriate for your application.
-        if (config('geonames.auto_load_migrations', false)) {
-            $this->loadMigrationsFrom( __DIR__ . '/Migrations' );
-        }
         $this->loadViewsFrom( __DIR__ . '/Views', 'geonames' );
 
 
--- src/config/geonames.php
+++ src/config/geonames.php
@@ -1,6 +1,5 @@
 <?php
 
 return [
-    'auto_load_migrations' => env( 'GEONAMES_AUTOLOAD_MIGRATIONS', false),
     'update_daily_at' => env( 'GEONAMES_UPDATE_DAILY_AT', '05:00' ),
 ];
